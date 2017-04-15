346aa4899add18f2c2118d7b53b0886c27e93fc9
Jessica Kerr <jessitron@gmail.com>
2017-04-15 10:28:13 -0500

    implement

--- .atomist/handlers/command/AddRestEndpointPlease.ts
+++ .atomist/handlers/command/AddRestEndpointPlease.ts
@@ -1,29 +1,56 @@
-import { HandleCommand, HandlerContext, ResponseMessage, Plan } from '@atomist/rug/operations/Handlers';
-import { CommandHandler, Parameter, Tags, Intent } from '@atomist/rug/operations/Decorators';
+import { HandleCommand, HandlerContext, MappedParameters, ResponseMessage, Plan } from '@atomist/rug/operations/Handlers';
+import { CommandHandler, MappedParameter, Parameter, Tags, Intent } from '@atomist/rug/operations/Decorators';
 import { Pattern } from '@atomist/rug/operations/RugOperation';
 
 /**
  * A run an editor to add a REST endpoint to this project.
  */
 @CommandHandler("AddRestEndpointPlease", "run an editor to add a REST endpoint to this project")
-@Tags("documentation")
+@Tags("satellite-of-love", "rest")
 @Intent("add endpoint")
 export class AddRestEndpointPlease implements HandleCommand {
 
+    @MappedParameter(MappedParameters.GITHUB_REPOSITORY)
+    repo: string;
+
     @Parameter({
-        displayName: "Some Input",
-        description: "example of how to specify a parameter using decorators",
-        pattern: Pattern.any,
-        validInput: "a description of the valid input",
+        displayName: "Java return type",
+        description: "type the endpoint will return",
+        pattern: Pattern.java_class,
+        validInput: "Java class name like ThunderCougarFalconBird",
         minLength: 1,
-        maxLength: 100,
-        required: false
+        maxLength: 100
     })
-    inputParameter: string = "default value";
+    returnedClass: string;
+
+    @Parameter({
+        displayName: "pojo field name",
+        description: "name of a field in your pojo, blank for none (or not a new pojo)",
+        pattern: Pattern.java_identifier,
+        validInput: "Java identifier",
+        minLength: 1,
+        maxLength: 100
+    })
+    fieldName: string = "";
+
+    @Parameter({
+        displayName: "pojo field type",
+        description: "type of that field in your pojo, blank for none (or not a new pojo)",
+        pattern: Pattern.any,
+        validInput: "Java type",
+        minLength: 1,
+        maxLength: 100
+    })
+    fieldType: string = "";
 
     handle(command: HandlerContext): Plan {
-        let message = new ResponseMessage(`Successfully ran AddRestEndpointPlease: ${this.inputParameter}`);
-        return Plan.ofMessage(message);
+        let edit = {
+            instruction: { kind: "edit", name: "AddRestEndpoint", project: this.repo, parameters: this },
+            onSuccess: new ResponseMessage(`I made a PR, you change it as you please.`)
+        };
+
+
+        return new Plan().add(edit);
     }
 }
 
