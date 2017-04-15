4866d0a1b542538065473c6b9b1eb2212971685b
Atomist <cli@atomist.com>
2017-04-15 15:13:19 -0500

    Adds a TypeScript Rug editor to a Rug project
    ```
    ---
    kind: "operation"
    client: "rug-cli 0.30.2"
    editor:
      name: "AddTypeScriptEditor"
      group: "atomist-rugs"
      artifact: "rug-editors"
      version: "0.25.0"
      origin:
        repo: "git@github.com:atomist-rugs/rug-editors.git"
        branch: "master"
        sha: "7557a1b"
      parameters:
        - "editorName": "UpgradeDockerfile"
        - "description": "make corrections to dockerfiles everywhere"
    
    ```

---
+++ .atomist/editors/UpgradeDockerfile.ts
@@ -0,0 +1,28 @@
+import { EditProject } from '@atomist/rug/operations/ProjectEditor';
+import { Project } from '@atomist/rug/model/Project';
+import { Pattern } from '@atomist/rug/operations/RugOperation';
+import { Editor, Parameter, Tags } from '@atomist/rug/operations/Decorators';
+
+/**
+ * Sample TypeScript editor used by AddUpgradeDockerfile.
+ */
+@Editor("UpgradeDockerfile", "make corrections to dockerfiles everywhere")
+@Tags("documentation")
+export class UpgradeDockerfile implements EditProject {
+
+    @Parameter({
+        displayName: "Some Input",
+        description: "example of how to specify a parameter using decorators",
+        pattern: Pattern.any,
+        validInput: "a description of the valid input",
+        minLength: 1,
+        maxLength: 100
+    })
+    inputParameter: string;
+
+    edit(project: Project) {
+        project.addFile("hello.txt", "Hello, World!\n" + this.inputParameter + "\n");
+    }
+}
+
+export const upgradeDockerfile = new UpgradeDockerfile();
