8e6004a15431259f139bb605702a748571dc8990
Jessica Kerr <jessitron@gmail.com>
2017-04-17 16:06:48 -0400

    a bit more

--- .atomist/editors/RestrictCORS.ts
+++ .atomist/editors/RestrictCORS.ts
@@ -10,16 +10,6 @@ import { Editor, Parameter, Tags } from '@atomist/rug/operations/Decorators';
 @Tags("documentation")
 export class RestrictCORS implements EditProject {
 
-    @Parameter({
-        displayName: "Some Input",
-        description: "example of how to specify a parameter using decorators",
-        pattern: Pattern.any,
-        validInput: "a description of the valid input",
-        minLength: 1,
-        maxLength: 100
-    })
-    inputParameter: string;
-
     edit(project: Project) {
         project.addFile("hello.txt", "Hello, World!\n" + this.inputParameter + "\n");
     }
