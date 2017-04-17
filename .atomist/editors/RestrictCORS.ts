b89e193e3f647ef24234cae910b97c546794c7b1
Jessica Kerr <jessitron@gmail.com>
2017-04-17 16:41:43 -0400

    changes

--- .atomist/editors/RestrictCORS.ts
+++ .atomist/editors/RestrictCORS.ts
@@ -1,17 +1,37 @@
 import { EditProject } from '@atomist/rug/operations/ProjectEditor';
 import { Project } from '@atomist/rug/model/Project';
+import { File } from '@atomist/rug/model/File';
 import { Pattern } from '@atomist/rug/operations/RugOperation';
 import { Editor, Parameter, Tags } from '@atomist/rug/operations/Decorators';
 
 /**
- * Sample TypeScript editor used by AddRestrictCORS.
+ * This implements a recommended security practice.
+ * 
+ * Teams may disregard or alter this change, as long as they comment about why.
  */
 @Editor("RestrictCORS", "Add a restriction to CrossOrigin annotations")
-@Tags("documentation")
+@Tags("spring", "satellite-of-love", "security")
 export class RestrictCORS implements EditProject {
 
     edit(project: Project) {
-        project.addFile("hello.txt", "Hello, World!\n" + this.inputParameter + "\n");
+
+        let pxe = project.context.pathExpressionEngine;
+
+        pxe.with<File>(project, "/src/main//File()", f => {
+            if (f.name.match(/.java$/)) {
+                f.replace("CrossOrigin()", `CrossOrigin(origins = "http://localhost:8080")`);
+            }
+        });
+
+        project.describeChange(`This is a recommended change from your friendly security advisers.
+        
+        While allowing cross-origin requests is essential for some local testing, it is 
+        otherwise discouraged by ... people. As long as your local front end is at localhost:8080,
+        you'll be able to test.
+
+        If there is a particular reason that your service should allow more requests (like, it's a public API)
+        then please comment on this PR and then close it or remove individual endpoints' changes.
+        `)
     }
 }
 
