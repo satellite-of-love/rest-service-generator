28aac7c51fdad6483175129c5d24d2e94c7a4753
Jessica Kerr <jessitron@gmail.com>
2017-04-25 19:19:09 -0700

    rod says this qbe will retrieve only one label

--- .atomist/handlers/event/RepoNoticer.ts
+++ .atomist/handlers/event/RepoNoticer.ts
@@ -1,10 +1,13 @@
 import { HandleEvent, DirectedMessage, ChannelAddress, EventPlan } from '@atomist/rug/operations/Handlers';
 import { Match } from '@atomist/rug/tree/PathExpression';
-import { EventHandler, Tags } from '@atomist/rug/operations/Decorators';
+import { EventHandler, Tags, Secrets } from '@atomist/rug/operations/Decorators';
 import { Repo } from "@atomist/cortex/stub/Repo";
+import { Label } from "@atomist/cortex/stub/Label";
+import { byExample } from "@atomist/rugs/util/tree/QueryByExample";
 
-@EventHandler("RepoNoticer", "does this work?", "/Repo()[/Label()]?")
+@EventHandler("RepoNoticer", "does this work?", byExample(new Repo().addLabels(new Label())))
 @Tags("repo")
+@Secrets("github://user_token?scopes=repo")
 export class RepoNoticer implements HandleEvent<Repo, Repo> {
     handle(event: Match<Repo, Repo>): EventPlan {
         let root: Repo = event.root();
