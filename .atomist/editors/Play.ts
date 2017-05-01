51485835feffec821d75b2d686a92ed821013af6
Jessica Kerr <jessitron@gmail.com>
2017-05-01 14:41:52 +1200

    Got contains working

--- .atomist/editors/Play.ts
+++ .atomist/editors/Play.ts
@@ -5,6 +5,7 @@ import { Pattern } from '@atomist/rug/operations/RugOperation';
 import { Editor, Parameter, Tags } from '@atomist/rug/operations/Decorators';
 import * as RugGeneratorFunctions from '../generators/RugGeneratorFunctions';
 import { RichTextTreeNode } from '@atomist/rug/ast/TextTreeNodeOps';
+import { TextTreeNode, PointFormatInfo } from '@atomist/rug/tree/PathExpression';
 import * as TreeHelper from '@atomist/rug/tree/TreeHelper';
 
 @Editor("Play", "learn about editors")
@@ -12,6 +13,7 @@ import * as TreeHelper from '@atomist/rug/tree/TreeHelper';
 export class Play implements EditProject {
 
     edit(project: Project) {
+        let positionOfInterest: Position = { line: 25, column: 12, };
         let filepathOfInterest = "src/test/java/com/atomist/springrest/addrestendpoint/OneEndpointWebIntegrationTests.java";
         let fileOfInterest = project.findFile(filepathOfInterest);
         if (fileOfInterest == null) {
@@ -22,7 +24,17 @@ export class Play implements EditProject {
 
         try {
             pxe.with<RichTextTreeNode>(fileOfInterest, "/JavaFile()", top => {
-                console.log(`Found top-level node: ${TreeHelper.stringify(top)}`);
+                console.log(`Found top-level node: ${top.nodeName()}`); // TODO: ask Rod - why is that still a function?
+
+                let someNode: TextTreeNode = top.children()[13] as TextTreeNode;
+
+                console.log(`Here is some interior node: ${TreeHelper.stringify(someNode)} at ${someNode.formatInfo}`);
+
+
+                console.log(`Does it contain ${JSON.stringify(positionOfInterest)}? ${contains(positionOfInterest, someNode)}`)
+                console.log(`Do you see it now? ${firstNodeThatContains(positionOfInterest, [someNode])}`)
+                console.log(`Can you see it in the children? ${firstNodeThatContains(positionOfInterest, top.children() as TextTreeNode[])}`)
+
             });
         }
         catch (e) {
@@ -30,8 +42,31 @@ export class Play implements EditProject {
         }
 
     }
+}
 
+interface Position { line: number, column: number }
 
+function firstNodeThatContains(pos: Position, nodes: TextTreeNode[]): TextTreeNode {
+    let c = nodes.filter(n => contains(pos, n))
+    if (c.length < 1) throw `no node contained it`
+    return c[0];
+}
+
+function after(start: PointFormatInfo, here: Position): boolean {
+    if (here.line < start.lineNumberFrom1) return false;
+    if (start.lineNumberFrom1 < here.line) return true;
+    if (here.line < start.columnNumberFrom1) return false;
+    return true;
+}
+
+function before(end: PointFormatInfo, here: Position): boolean {
+    if (end.lineNumberFrom1 < here.line) return false;
+    if (here.line < end.lineNumberFrom1) return true;
+    if (end.columnNumberFrom1 < here.column) return false;
+    return true;
+}
+function contains(pos: Position, node: TextTreeNode): boolean {
+   return after(node.formatInfo.start, pos) && before(node.formatInfo.end, pos);
 }
 
 export const play = new Play();
