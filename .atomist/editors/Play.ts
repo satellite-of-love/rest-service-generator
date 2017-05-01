143ac17baee4067283979db431721ae2be158749
Jessica Kerr <jessitron@gmail.com>
2017-05-01 14:05:07 +1200

    I can catch Scala exceptions in ts
    The exception for root-node-is-null is not pretty though

--- .atomist/editors/Play.ts
+++ .atomist/editors/Play.ts
@@ -4,22 +4,31 @@ import { File } from '@atomist/rug/model/File';
 import { Pattern } from '@atomist/rug/operations/RugOperation';
 import { Editor, Parameter, Tags } from '@atomist/rug/operations/Decorators';
 import * as RugGeneratorFunctions from '../generators/RugGeneratorFunctions';
-import { RichTextTreeNode } from '@atomist/rug/ast/TextTreeNodeOps'
+import { RichTextTreeNode } from '@atomist/rug/ast/TextTreeNodeOps';
+import * as TreeHelper from '@atomist/rug/tree/TreeHelper';
 
 @Editor("Play", "learn about editors")
-@Tags( "satellite-of-love", "jess")
+@Tags("satellite-of-love", "jess")
 export class Play implements EditProject {
 
     edit(project: Project) {
-       let filepathOfInterest = "src/test/java/com/jessitron/addrestendpoint/OneEndpointWebIntegrationTest.java";
-       let fileOfInterest = project.findFile(filepathOfInterest);
+        let filepathOfInterest = "src/test/java/com/atomist/springrest/addrestendpoint/OneEndpointWebIntegrationTests.java";
+        let fileOfInterest = project.findFile(filepathOfInterest);
+        if (fileOfInterest == null) {
+            throw `File not found: ${filepathOfInterest}`;
+        }
 
-       let pxe = project.context.pathExpressionEngine;
+        let pxe = project.context.pathExpressionEngine;
 
-       pxe.with<RichTextTreeNode>(fileOfInterest, "/JavaFile()", top => {
-           console.log(`Found top-level node: ${top.nodeName}`);
-       })
-   
+        try {
+            pxe.with<RichTextTreeNode>(fileOfInterest, "/JavaFile()", top => {
+                console.log(`Found top-level node: ${TreeHelper.stringify(top)}`);
+            });
+        }
+        catch (e) {
+            console.log(`problem: ${e}`);
+        }
+
     }
 
 
