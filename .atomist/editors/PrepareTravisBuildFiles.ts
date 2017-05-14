1658493513eef713ac9aae5e5e129a17aba3fa57
Jessica Kerr <jessitron@gmail.com>
2017-05-14 15:22:04 -0500

    tslinting

--- .atomist/editors/PrepareTravisBuildFiles.ts
+++ .atomist/editors/PrepareTravisBuildFiles.ts
@@ -1,16 +1,16 @@
-import { EditProject } from '@atomist/rug/operations/ProjectEditor';
-import { Project } from '@atomist/rug/model/Project';
-import { Pattern } from '@atomist/rug/operations/RugOperation';
-import { Editor, Parameter, Tags } from '@atomist/rug/operations/Decorators';
-import { PathExpressionEngine } from '@atomist/rug/tree/PathExpression';
-import { Pom } from '@atomist/rug/model/Pom';
+import { Pom } from "@atomist/rug/model/Pom";
+import { Project } from "@atomist/rug/model/Project";
+import { Editor, Parameter, Tags } from "@atomist/rug/operations/Decorators";
+import { EditProject } from "@atomist/rug/operations/ProjectEditor";
+import { Pattern } from "@atomist/rug/operations/RugOperation";
+import { PathExpressionEngine } from "@atomist/rug/tree/PathExpression";
 
-let encryptedTokenParameterValidation = {
-	pattern: Pattern.any,
-	validInput: "alphanumeric",
-	minLength: 1,
-	maxLength: 1000
-}
+const encryptedTokenParameterValidation = {
+    pattern: Pattern.any,
+    validInput: "alphanumeric",
+    minLength: 1,
+    maxLength: 1000,
+};
 /**
  * Sample TypeScript editor used by AddMyFirstEditor.
  */
@@ -18,132 +18,131 @@ let encryptedTokenParameterValidation = {
 @Tags("satellite-of-love", "travis")
 export class PrepareTravisBuildFiles implements EditProject {
 
+    @Parameter({
+        ...encryptedTokenParameterValidation,
+        displayName: "encrypted github token",
+        description: "travis-encrypted GI	THUB_TOKEN=...",
+    })
+    public encryptedGithubToken: string;
 
-	@Parameter({
-		...encryptedTokenParameterValidation,
-		displayName: "encrypted github token",
-		description: "travis-encrypted GITHUB_TOKEN=..."
-	})
-	encryptedGithubToken: string;
+    @Parameter({
+        ...encryptedTokenParameterValidation,
+        displayName: "encrypted atomist docker repo user",
+        description: "travis-encrypted ATOMIST_REPO_USER=...",
+    })
+    public encryptedDockerRegistryUser: string;
 
-	@Parameter({
-		...encryptedTokenParameterValidation,
-		displayName: "encrypted atomist docker repo user",
-		description: "travis-encrypted ATOMIST_REPO_USER=..."
-	})
-	encryptedDockerRegistryUser: string;
+    @Parameter({
+        ...encryptedTokenParameterValidation,
+        displayName: "encrypted atomist docker token",
+        description: "travis-encrypted ATOMIST_REPO_TOKEN=...",
+    })
+    public encryptedDockerRegistryToken: string;
 
-	@Parameter({
-		...encryptedTokenParameterValidation,
-		displayName: "encrypted atomist docker token",
-		description: "travis-encrypted ATOMIST_REPO_TOKEN=..."
-	})
-	encryptedDockerRegistryToken: string;
+    public edit(project: Project) {
+        console.log(`Enabling build on ${project.name}`);
 
-	edit(project: Project) {
-		console.log(`Enabling build on ${project.name}`);
+        // .travis.yml
+        if (!project.fileExists(".travis.yml")) {
+            project.copyEditorBackingFileOrFail(".travis.yml");
+        }
 
-		// .travis.yml
-		if (!project.fileExists(".travis.yml")) {
-			project.copyEditorBackingFileOrFail(".travis.yml");
-		}
-
-		// replace all secrets
-		let travisFile = project.findFile(".travis.yml");
-		let newContent = travisFile.content.replace(
-			/\n.*- secure: .*\n/g, "\n").replace(
-			"  global:",
-			`  global:
+        // replace all secrets
+        const travisFile = project.findFile(".travis.yml");
+        const newContent = travisFile.content.replace(
+            /\n.*- secure: .*\n/g, "\n").replace(
+            "  global:",
+            `  global:
   - secure: ${this.encryptedDockerRegistryToken}
   - secure: ${this.encryptedDockerRegistryUser}
   - secure: ${this.encryptedGithubToken}`);
         travisFile.setContent(newContent);
 
-		// docker
-		if (!project.fileExists("src/main/docker/Dockerfile")) {
-			project.copyEditorBackingFileOrFail("src/main/docker/Dockerfile")
-		}
+        // docker
+        if (!project.fileExists("src/main/docker/Dockerfile")) {
+            project.copyEditorBackingFileOrFail("src/main/docker/Dockerfile");
+        }
 
-		// build script
-		if (!project.fileExists("src/main/scripts/travis-build.bash")) {
-			project.copyEditorBackingFileOrFail("src/main/scripts/travis-build.bash");
-		}
+        // build script
+        if (!project.fileExists("src/main/scripts/travis-build.bash")) {
+            project.copyEditorBackingFileOrFail("src/main/scripts/travis-build.bash");
+        }
 
-		// update the POM
-		let eng: PathExpressionEngine = project.context.pathExpressionEngine;
-		eng.with<Pom>(project, "/Pom()", pom => {
-			pom.addOrReplaceBuildPlugin("org.apache.maven.plugins", "maven-dependency-plugin",
-				`<plugin>
-				<groupId>org.apache.maven.plugins</groupId>
-				<artifactId>maven-dependency-plugin</artifactId>
-				<version>2.10</version>
-				<executions>
-					<execution>
-						<id>unpack</id>
-						<phase>package</phase>
-						<goals>
-							<goal>unpack</goal>
-						</goals>
-						<configuration>
-							<artifactItems>
-								<artifactItem>
-									<groupId>\${project.groupId}</groupId>
-									<artifactId>\${project.artifactId}</artifactId>
-									<version>\${project.version}</version>
-									<type>jar</type>
-									<outputDirectory>\${project.build.directory}/docker</outputDirectory>
-								</artifactItem>
-							</artifactItems>
-						</configuration>
-					</execution>
-				</executions>
-			</plugin>`);
-			pom.addOrReplaceBuildPlugin("org.apache.maven.plugins", "maven-resources-plugin", `
-			<plugin>
-				<groupId>org.apache.maven.plugins</groupId>
-				<artifactId>maven-resources-plugin</artifactId>
-				<executions>
-					<execution>
-						<id>prepare-dockerfile</id>
-						<phase>validate</phase>
-						<goals>
-							<goal>copy-resources</goal>
-						</goals>
-						<configuration>
-							<outputDirectory>\${project.build.directory}/docker-filtered</outputDirectory>
-							<resources>
-								<resource>
-									<directory>src/main/docker</directory>
-									<filtering>true</filtering>
-								</resource>
-							</resources>
-						</configuration>
-					</execution>
-				</executions>
-			</plugin>`);
-			pom.addOrReplaceBuildPlugin("com.spotify", "docker-maven-plugin", `
-			<plugin>
-				<groupId>com.spotify</groupId>
-				<artifactId>docker-maven-plugin</artifactId>
-				<version>0.4.12</version>
-				<configuration>
-					<imageName>sforzando-docker-dockerv2-local.artifactoryonline.com/\${project.name}</imageName>
-					<dockerDirectory>\${project.build.directory}/docker-filtered</dockerDirectory>
-					<resources>
-						<resource>
-							<targetPath>/</targetPath>
-							<directory>\${project.build.directory}</directory>
-							<include>\${project.build.finalName}.jar</include>
-						</resource>
-					</resources>
-					<useConfigFile>true</useConfigFile>
-					<imageTags>
-						<imageTag>\${project.version}</imageTag>
-					</imageTags>
-				</configuration>
-			</plugin>`);
-		});
-	}
+        // update the POM
+        const eng: PathExpressionEngine = project.context.pathExpressionEngine;
+        eng.with<Pom>(project, "/Pom()", (pom) => {
+            pom.addOrReplaceBuildPlugin("org.apache.maven.plugins", "maven-dependency-plugin",
+                `<plugin>
+                <groupId>org.apache.maven.plugins</groupId>
+                <artifactId>maven-dependency-plugin</artifactId>
+                <version>2.10</version>
+                <executions>
+                    <execution>
+                        <id>unpack</id>
+                        <phase>package</phase>
+                        <goals>
+                            <goal>unpack</goal>
+                        </goals>
+                        <configuration>
+                            <artifactItems>
+                                <artifactItem>
+                                    <groupId>\${project.groupId}</groupId>
+                                    <artifactId>\${project.artifactId}</artifactId>
+                                    <version>\${project.version}</version>
+                                    <type>jar</type>
+                                    <outputDirectory>\${project.build.directory}/docker</outputDirectory>
+                                </artifactItem>
+                            </artifactItems>
+                        </configuration>
+                    </execution>
+                </executions>
+            </plugin>`);
+            pom.addOrReplaceBuildPlugin("org.apache.maven.plugins", "maven-resources-plugin", `
+            <plugin>
+                <groupId>org.apache.maven.plugins</groupId>
+                <artifactId>maven-resources-plugin</artifactId>
+                <executions>
+                    <execution>
+                        <id>prepare-dockerfile</id>
+                        <phase>validate</phase>
+                        <goals>
+                            <goal>copy-resources</goal>
+                        </goals>
+                        <configuration>
+                            <outputDirectory>\${project.build.directory}/docker-filtered</outputDirectory>
+                            <resources>
+                                <resource>
+                                    <directory>src/main/docker</directory>
+                                    <filtering>true</filtering>
+                                </resource>
+                            </resources>
+                        </configuration>
+                    </execution>
+                </executions>
+            </plugin>`);
+            pom.addOrReplaceBuildPlugin("com.spotify", "docker-maven-plugin", `
+            <plugin>
+                <groupId>com.spotify</groupId>
+                <artifactId>docker-maven-plugin</artifactId>
+                <version>0.4.12</version>
+                <configuration>
+                    <imageName>sforzando-docker-dockerv2-local.artifactoryonline.com/\${project.name}</imageName>
+                    <dockerDirectory>\${project.build.directory}/docker-filtered</dockerDirectory>
+                    <resources>
+                        <resource>
+                            <targetPath>/</targetPath>
+                            <directory>\${project.build.directory}</directory>
+                            <include>\${project.build.finalName}.jar</include>
+                        </resource>
+                    </resources>
+                    <useConfigFile>true</useConfigFile>
+                    <imageTags>
+                        <imageTag>\${project.version}</imageTag>
+                    </imageTags>
+                </configuration>
+            </plugin>`);
+        });
+    }
 }
 
 export const prepareTravisBuildFiles = new PrepareTravisBuildFiles();
--- .atomist/handlers/command/EnableTravisBuild.ts
+++ .atomist/handlers/command/EnableTravisBuild.ts
@@ -1,18 +1,34 @@
-import { HandleCommand, Response, HandleResponse, HandlerContext, ResponseMessage, CommandPlan } from '@atomist/rug/operations/Handlers';
-import { EventHandler, ResponseHandler, ParseJson, CommandHandler, Secrets, MappedParameter, Parameter, Tags, Intent } from '@atomist/rug/operations/Decorators'
-import { Pattern } from '@atomist/rug/operations/RugOperation';
-import * as PlanUtils from '@atomist/rugs/operations/PlanUtils';
-import * as CommonHandlers from '@atomist/rugs/operations/CommonHandlers';
+import {
+    CommandHandler,
+    EventHandler,
+    Intent,
+    MappedParameter,
+    Parameter,
+    ParseJson,
+    ResponseHandler,
+    Secrets,
+    Tags,
+} from "@atomist/rug/operations/Decorators";
+import {
+    CommandPlan,
+    HandleCommand,
+    HandlerContext,
+    HandleResponse,
+    Response,
+    ResponseMessage,
+} from "@atomist/rug/operations/Handlers";
+import {Pattern} from "@atomist/rug/operations/RugOperation";
+import * as CommonHandlers from "@atomist/rugs/operations/CommonHandlers";
+import * as PlanUtils from "@atomist/rugs/operations/PlanUtils";
 
-
-let githubRepoParameter = {
+const githubRepoParameter = {
     displayName: "github repository",
     description: "a repository to enable builds on",
     pattern: Pattern.any,
     validInput: "repo name",
     minLength: 1,
-    maxLength: 100
-}
+    maxLength: 100,
+};
 
 /**
  * A sample Rug TypeScript command handler.
@@ -24,65 +40,69 @@ let githubRepoParameter = {
 class EnableTravisBuild implements HandleCommand {
 
     @Parameter(githubRepoParameter)
-    repo: string;
+    public repo: string;
 
-    handle(command: HandlerContext): CommandPlan {
+    public handle(command: HandlerContext): CommandPlan {
         console.log(`We would like to turn on builds for ${this.repo}`);
 
-        let executeTravisEnableRepo = PlanUtils.execute("travis-enable-repo",
+        const executeTravisEnableRepo = PlanUtils.execute("travis-enable-repo",
             {
                 repo: this.repo,
                 owner: "satellite-of-love",
-                org: ".org"
-            }
+                org: ".org",
+            },
         );
-        let encryptGithubSecret = encryptInstruction(this.repo, `GITHUB_TOKEN=#{github://user_token?scopes=repo}`)
-        encryptGithubSecret.onSuccess = ({ kind: "respond", name: "ReceiveGithubToken", parameters: { repo: this.repo } })
+        const encryptGithubSecret = encryptInstruction(this.repo, `GITHUB_TOKEN=#{github://user_token?scopes=repo}`);
+        encryptGithubSecret.onSuccess = (
+            {
+                kind: "respond",
+                name: "ReceiveGithubToken",
+                parameters: {repo: this.repo},
+            });
 
-        let encryptSecretsAndAddBuildFiles = new CommandPlan();
+        const encryptSecretsAndAddBuildFiles = new CommandPlan();
         encryptSecretsAndAddBuildFiles.add(encryptGithubSecret);
-        encryptSecretsAndAddBuildFiles.add(new ResponseMessage(`1: Enabled build for ${this.repo} on travis-ci.org`))
+        encryptSecretsAndAddBuildFiles.add(new ResponseMessage(`1: Enabled build for ${this.repo} on travis-ci.org`));
         executeTravisEnableRepo.onSuccess = encryptSecretsAndAddBuildFiles;
         // CommonHandlers.handleErrors(executeTravisEnableRepo, "Failure enabling build on Travis.")
 
-        let message = new ResponseMessage(`There are 5 steps to enabling a Travis build:`);
-        let plan = CommandPlan.ofMessage(message);
+        const message = new ResponseMessage(`There are 5 steps to enabling a Travis build:`);
+        const plan = CommandPlan.ofMessage(message);
         plan.add(executeTravisEnableRepo);
         return plan;
     }
-
 
 }
 
 function encryptInstruction(repo: string, content: string) {
     return PlanUtils.execute("travis-encrypt",
         {
-            repo: repo,
+            repo,
             owner: "satellite-of-love",
             org: ".org",
-            content: content
-        }
-    )
+            content,
+        },
+    );
 }
-
 
 @ResponseHandler("ReceiveGithubToken", "step 2")
 @Secrets("github://user_token?scopes=repo", "secret://team?path=/docker/token")
-class ReceiveGithubToken implements HandleResponse<any>{
+class ReceiveGithubToken implements HandleResponse<any> {
 
     @Parameter(githubRepoParameter)
-    repo: string;
+    public repo: string;
 
-    handle(response: Response<string>, ): CommandPlan {
-        let plan = CommandPlan.ofMessage(new ResponseMessage("2: Encrypted Github Token for travis"));
-        let encryptDockerToken = encryptInstruction(this.repo, `ATOMIST_REPO_TOKEN=#{secret://team?path=/docker/token}`)
+    public handle(response: Response<string>): CommandPlan {
+        const plan = CommandPlan.ofMessage(new ResponseMessage("2: Encrypted Github Token for travis"));
+        const encryptDockerToken = encryptInstruction(this.repo,
+            `ATOMIST_REPO_TOKEN=#{secret://team?path=/docker/token}`);
         encryptDockerToken.onSuccess = ({
             kind: "respond", name: "ReceiveDockerToken",
             parameters: {
                 repo: this.repo,
-                encryptedGithubToken: response.body
-            }
-        })
+                encryptedGithubToken: response.body,
+            },
+        });
         plan.add(encryptDockerToken);
         return plan;
     }
@@ -90,9 +110,9 @@ class ReceiveGithubToken implements HandleResponse<any>{
 
 @ResponseHandler("ReceiveDockerToken", "step 3")
 @Secrets("github://user_token?scopes=repo")
-class ReceiveDockerToken implements HandleResponse<any>{
+class ReceiveDockerToken implements HandleResponse<any> {
     @Parameter(githubRepoParameter)
-    repo: string;
+    public repo: string;
 
     @Parameter({
         displayName: "don't display this",
@@ -100,22 +120,23 @@ class ReceiveDockerToken implements HandleResponse<any>{
         pattern: Pattern.any,
         validInput: "long string of nonsense",
         minLength: 1,
-        maxLength: 1000
+        maxLength: 1000,
     })
-    encryptedGithubToken: string;
+    public encryptedGithubToken: string;
 
-    handle(response: Response<string>): CommandPlan {
-        let encryptedDockerToken: string = response.body;
-        let plan = CommandPlan.ofMessage(new ResponseMessage(`3: Encrypted Docker registry token for travis`));
-        let encryptDockerUser = encryptInstruction(this.repo, `ATOMIST_REPO_USER=travis-docker`) // this is not exactly secret but we don't want it scrapeable from the .travis.yml
+    public handle(response: Response<string>): CommandPlan {
+        const encryptedDockerToken: string = response.body;
+        const plan = CommandPlan.ofMessage(new ResponseMessage(`3: Encrypted Docker registry token for travis`));
+        // this next one is not exactly secret but we don't want it scrapeable from the .travis.yml
+        const encryptDockerUser = encryptInstruction(this.repo, `ATOMIST_REPO_USER=travis-docker`);
         encryptDockerUser.onSuccess = ({
             kind: "respond", name: "ReceiveDockerUser",
             parameters: {
                 repo: this.repo,
                 encryptedGithubToken: this.encryptedGithubToken,
-                encryptedDockerToken: encryptedDockerToken
-            }
-        })
+                encryptedDockerToken,
+            },
+        });
         plan.add(encryptDockerUser);
         return plan;
     }
@@ -123,9 +144,9 @@ class ReceiveDockerToken implements HandleResponse<any>{
 
 @ResponseHandler("ReceiveDockerUser", "step 4")
 @Secrets("github://user_token?scopes=repo")
-class ReceiveDockerUser implements HandleResponse<any>{
+class ReceiveDockerUser implements HandleResponse<any> {
     @Parameter(githubRepoParameter)
-    repo: string;
+    public repo: string;
 
     @Parameter({
         displayName: "don't display this",
@@ -133,9 +154,9 @@ class ReceiveDockerUser implements HandleResponse<any>{
         pattern: Pattern.any,
         validInput: "long string of nonsense",
         minLength: 1,
-        maxLength: 1000
+        maxLength: 1000,
     })
-    encryptedGithubToken: string;
+    public encryptedGithubToken: string;
 
     @Parameter({
         displayName: "don't display this",
@@ -143,35 +164,35 @@ class ReceiveDockerUser implements HandleResponse<any>{
         pattern: Pattern.any,
         validInput: "long string of nonsense",
         minLength: 1,
-        maxLength: 1000
+        maxLength: 1000,
     })
-    encryptedDockerToken: string;
+    public encryptedDockerToken: string;
 
-    handle(response: Response<string>): CommandPlan {
-        let encryptedDockerUser: string = response.body
-        let plan = CommandPlan.ofMessage(new ResponseMessage("4: Encrypted Docker token for Travis"));
-        let editorName = "PrepareTravisBuildFiles";
-        let editorParameters = {
+    public handle(response: Response<string>): CommandPlan {
+        const encryptedDockerUser: string = response.body;
+        const plan = CommandPlan.ofMessage(new ResponseMessage("4: Encrypted Docker token for Travis"));
+        const editorName = "PrepareTravisBuildFiles";
+        const editorParameters = {
             project: this.repo,
             encryptedGithubToken: this.encryptedGithubToken,
             encryptedDockerRegistryToken: this.encryptedDockerToken,
-            encryptedDockerRegistryUser: encryptedDockerUser
+            encryptedDockerRegistryUser: encryptedDockerUser,
         };
-        let prepareBuildFiles = {
+        const prepareBuildFiles = {
             instruction: {
                 kind: "edit", name: editorName,
                 project: this.repo,
-                parameters: editorParameters
+                parameters: editorParameters,
             },
-            onSuccess: CommandPlan.ofMessage(new ResponseMessage("5: Added Travis build files to the repository. Please accept my PR!"))
-        }
+            onSuccess: CommandPlan.ofMessage(
+                new ResponseMessage("5: Added Travis build files to the repository. Please accept my PR!")),
+        };
         plan.add(prepareBuildFiles);
         return plan;
     }
 }
 
-
-export { EnableTravisBuild }
+export {EnableTravisBuild};
 export const enableTravisBuild = new EnableTravisBuild();
 export const receiveGithubToken = new ReceiveGithubToken();
 export const receiveDockerToken = new ReceiveDockerToken();
