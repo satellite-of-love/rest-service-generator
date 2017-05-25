f47e2c93771e594b718ece7d44bf1308c1b58304
Jessica Kerr <jessitron@gmail.com>
2017-05-25 14:49:23 -0500

    formatting

--- .atomist/editors/CommonResponseHandlers.ts
+++ .atomist/editors/CommonResponseHandlers.ts
@@ -1,4 +1,4 @@
-import * as CommonHandlers from "@atomist/rugs/operations/CommonHandlers"
+import * as CommonHandlers from "@atomist/rugs/operations/CommonHandlers";
 
 export const failer = new CommonHandlers.GenericErrorHandler();
 export const hooray = new CommonHandlers.GenericSuccessHandler();
